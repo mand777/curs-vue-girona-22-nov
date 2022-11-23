fc8138a3630605a37b6e3d47d15984016fcfa2d9
mand777 <amandvieira7@gmail.com>
2022-11-23 17:41:48 +0100

    caractersdiferents

--- codi/components/loginUsuari.vue
+++ codi/components/loginUsuari.vue
@@ -3,24 +3,81 @@
         <v-row>
             <v-col style="margin-top:30vh" cols="12 md=4" offset-md="4">
                 <v-text-field
-                    label="usuari">
+                    label="usuari"
+                    v-model="usuari">
                 </v-text-field>
-
 
                 <v-text-field
                     label="password"
-                    type="password">
+                    type="password"
+                    v-model="contrasenya">
+
                 </v-text-field>
-                <div style="color:green">Molt bé, tens majúscules i minúscules</div>
-                <div style="color:red">Ep, cal posar alguna majúscula i minúscula</div>
-                <div style="color:green">Molt bé, tens més de 6 caràcters</div>
-                <div style="color:red">Cal posar mínim 6 caràcters</div>
-                <div style="color:green">Molt bé, tens algun caràcter especial</div>
-                <div style="color:red">Cal posar algun caràcter especial</div>
+                <div v-if="siHiHaMajusculesMinuscules" style="color:green">Molt bé, tens majúscules i minúscules</div>
+                <div v-else style="color:red">Ep, cal posar alguna majúscula i minúscula</div>
+                <div v-if="siHiHaMesDe6Caracters" style="color:green">Molt bé, tens més de 6 caràcters</div>
+                <div v-else style="color:red">Cal posar mínim 6 caràcters</div>
+                <div v-if="siHiHaCaractersEspecials" style="color:green">Molt bé, tens algun caràcter especial</div>
+                <div v-else style="color:red">Cal posar algun caràcter especial</div>
+                <v-btn>login</v-btn>
 
             </v-col>
         </v-row>
     </v-container>
-   
-    
 </template>
+
+<script>
+
+export default{
+    data(){
+        return{
+            usuari:"",
+            contrasenya:""
+
+        }
+    },
+    methods:{
+        conteLletres(str){
+            return /[a-zA-Z]/.test(str)
+        }
+
+    },
+
+    computed:{
+        siHiHaMajusculesMinuscules(){
+            let textOriginal=this.contrasenya
+            let hihaMajuscules=false
+            let hihaMinuscules=false
+
+            for(var i=0;i<textOriginal.length;i++){
+                let caracter=textOriginal[i]
+             if(this.conteLletres(caracter)){
+
+                if(caracter.toUpperCase()===caracter){
+                    hihaMajuscules=true
+                }else{
+                    hihaMinuscules=true
+                }
+            }
+        
+            }
+            return hihaMajuscules&&hihaMinuscules
+ 
+        },
+        siHiHaMesDe6Caracters(){
+            // fem algo i tornem a true si hi ha mes de 6 car
+            if(this.contrasenya
+            &&typeof(this.contrasenya)=="string"
+            &&this.contrasenya.length>=6){
+                return true}
+             //tornem fals
+            return false
+
+        },
+        siHiHaCaractersEspecials(){
+            return false
+
+        }
+    } 
+}
+</script>
